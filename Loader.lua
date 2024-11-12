e74fdb88478b25ac0441bb6086616d1e8c1a705b
saifaimizumi <168169720+saifaimizumi@users.noreply.github.com>
2024-11-12 09:27:58 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,61 +1,22 @@
---lyxme Hub
-function TeleportToPlace(Place)
-    if Place == "Spawn" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1.47872591, 3.2980268, 12.920928, -0.130765319, 3.29837455e-08, -0.991413355, 9.6493622e-09, 1, 3.1996688e-08, 0.991413355, -5.38245004e-09, -0.130765319)
-    elseif Place == "Subway" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(50.600769, -12.5530424, 56.7129555, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "Coffee" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(156.481567, 15.290041, 35.5562515, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "CementBuilding" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-80.4957581, -6.35995483, 259.231842, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "Club" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(185.138092, 26.2499943, -180.177734, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "Plane" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(465.365082, -106.600021, 117.17836, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "Church" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-66.611908, 40.6999855, -192.577744, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "Airport" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(310.838959, 14.3999968, 53.5641975, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "Police" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-80.412178, 30.044632, 27.2221699, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "CHildCare" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-132.412186, 14.7700405, 114.882195, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "School" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-346.612518, 22.9000282, 251.472137, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "Carwash" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(245.13765, 8.09999084, -296.677643, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "Motel" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(159.088257, 3.39802194, 212.383179, 0.700654268, -1.81963475e-08, -0.713500917, 4.44143566e-09, 1, -2.11414406e-08, 0.713500917, 1.16438725e-08, 0.700654268)
-    elseif Place == "AgencyPool" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-53.874279, -21.3401012, 110.148872, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "Yacht" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-124.410553, 16.7000046, 856.409302, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    elseif Place == "Cabin" then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-269.829865, 10.7892895, 1105.65161, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-    end
-end
-
+--lyxme Hub | Brookhaven
 
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "lyxme Hub | Brookhaven 7 November 2024",
+    Title = "lyxme Hub | Brookhaven",
     SubTitle = "",
     TabWidth = 160,
     Size = UDim2.fromOffset(510, 390),
     Acrylic = false, -- The blur may be detectable, setting this to false disables blur entirely
-    Theme = "Amethyst",
+    Theme = "Amethystr",
     MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
 
 --Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
 local Tabs = {
-    Genaral = Window:AddTab({ Title = "Genaral Tab", Icon = "rbxassetid://11433532654" }),
-    Avatar = Window:AddTab({ Title = "Avatar Tab",Icon = "shirt" }),
-    Player = Window:AddTab({ Title = "Player",Icon = "user" }),
-    Teleport = Window:AddTab({ Title = "Teleport",Icon = "palmtree" }),
+    Genaral = Window:AddTab({ Title = "Genaral", Icon = "rbxassetid://11433532654" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
 
@@ -64,7 +25,7 @@ local Options = Fluent.Options
 do
     Fluent:Notify({
         Title = "Notification",
-        Content = "lyxme Hub running script",
+        Content = "lyxme Hub No.1",
         SubContent = "", -- Optional
         Duration = 10 -- Set to nil to make the notification not disappear
     })
@@ -72,155 +33,58 @@ do
 end
 
 
-            
-                                 
-
-local Toggle = Tabs.Genaral:AddToggle("MyToggle", {Title = "Auto...", Default = false })
-
-Toggle:OnChanged(function(Value)
-end)
-Options.MyToggle:SetValue(false)
-
-
-local Toggle = Tabs.Genaral:AddToggle("MyToggle", {Title = "Auto...", Default = false })
-
-    Toggle:OnChanged(function(Value)
-        _G.antiAFK = Value
-
-        while _G.antiAFK do wait(20)
-    
-        game:GetService'VirtualUser':Button1Down(Vector2.new(788, 547))
-        
+local Slider = Tabs.Genaral:AddSlider("Slider", {
+    Title = "Speed",
+    Description = "",
+    Default = 20,
+    Min = 0,
+    Max = 1000000,
+    Rounding = 0,
+    Callback = function(Value)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
     end
-    end)
-
-    Options.MyToggle:SetValue(false)
-
-local ToggleMelee = Tabs.Avatar:AddToggle("ToggleMelee", {Title = "Melee",Description = "", Default = false })
-ToggleMelee:OnChanged(function(Value)
-    _G.Auto_Stats_Melee = Value
-    end)
-Options.ToggleMelee:SetValue(false)
-
-
-
-local ToggleDe = Tabs.Avatar:AddToggle("ToggleDe", {Title = "Defense",Description = "", Default = false })
-ToggleDe:OnChanged(function(Value)
-    _G.Auto_Stats_Defense = Value
-    end)
-Options.ToggleDe:SetValue(false)
-
-
-
-local ToggleSword = Tabs.Avatar:AddToggle("ToggleSword", {Title = "Sword",Description = "", Default = false })
-ToggleSword:OnChanged(function(Value)
-    _G.Auto_Stats_Sword = Value
-    end)
-Options.ToggleSword:SetValue(false)
-
-
-
-local ToggleGun = Tabs.Avatar:AddToggle("ToggleGun", {Title = "Gun", Description = "",Default = false })
-ToggleGun:OnChanged(function(Value)
-    _G.Auto_Stats_Gun = Value
-    end)
-Options.ToggleGun:SetValue(false)
-
-
-local ToggleFruit = Tabs.Avatar:AddToggle("ToggleFruit", {Title = "Blox Fruit",Description = "", Default = false })
-ToggleFruit:OnChanged(function(Value)
-    _G.Auto_Stats_Devil_Fruit = Value
-    end)
-Options.ToggleFruit:SetValue(false)
-
-
-spawn(function()
-    while wait() do
-        if _G.Auto_Stats_Devil_Fruit then
-            local args = {
-                [1] = "AddPoint",
-                [2] = "Demon Fruit",
-                [3] = 3
-            }
-                        
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-        end
-    end
-end)
-
-spawn(function()
-    while wait() do
-        if _G.Auto_Stats_Gun then
-            local args = {
-                [1] = "AddPoint",
-                [2] = "Gun",
-                [3] = 3
-            }
-                        
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-        end
-    end
-end)
-
-
-spawn(function()
-    while wait() do
-        if _G.Auto_Stats_Sword then
-            local args = {
-                [1] = "AddPoint",
-                [2] = "Sword",
-                [3] = 3
-            }
-                        
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-        end
-    end
-end)
-
-spawn(function()
-    while wait() do
-        if _G.Auto_Stats_Defense then
-            local args = {
-                [1] = "AddPoint",
-                [2] = "Defense",
-                [3] = 3
-            }
-                        
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-        end
-    end
-end)
-
-
-spawn(function()
-    while wait() do
-        if _G.Auto_Stats_Melee then
-            local args = {
-                [1] = "AddPoint",
-                [2] = "Melee",
-                [3] = 3
-            }
-                        
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-        end
-    end
-end)
-
-
-local Dropdown = Tabs.Teleport:AddDropdown("Dropdown", {
-    Title = "Select Place",
-    Values = {"Spawn", "Subway", "Coffee", "CementBuilding", "Club", "Plane", "Church", "Airport", "Police," "CHildCare", "School", "Carwash", "Motel", "AgencyPool", "Yacht", "Cabin"},
-    Multi = false,
-    Default = 1,
 })
 
-Dropdown:OnChanged(function(Value)
-    Place = Value
+Slider:OnChanged(function(Value)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
 end)
+
+Slider:SetValue(16)
+
+--Jump Power
+
+local Slider = Tabs.Genaral:AddSlider("Slider", {
+    Title = "JumpPower",
+    Description = "",
+    Default = 50,
+    Min = 15,
+    Max = 1000000,
+    Rounding = 0,
+    Callback = function(Value)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+    end
+})
+
+Slider:OnChanged(function(Value)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+end)
+
+Slider:SetValue(50)
+
+
+Tabs.Genaral:AddButton({
+        Title = "Fly Script",
+        Description = "",
+        Callback = function()
+            loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
+        end
+    })
+
+
 
 
     Tabs.Settings:AddButton({
-        Title = "Rejoin Server",
+        Title = "rejoin server",
         Description = "",
         Callback = function()
             local ts = game:GetService("TeleportService")
@@ -261,6 +125,8 @@ end)
     
     
     
+    
+
 -- Addons:
 -- SaveManager (Allows you to have a configuration system)
 -- InterfaceManager (Allows you to have a interface managment system)
@@ -288,10 +154,45 @@ Window:SelectTab(1)
 
 Fluent:Notify({
     Title = "Notification",
-    Content = "The script has been loading",
+    Content = "The script has been loaded.",
     Duration = 5
 })
 
 -- You can use the SaveManager:LoadAutoloadConfig() to load a config
 -- which has been marked to be one that auto loads!
 SaveManager:LoadAutoloadConfig()
+
+
+--uitoggle
+
+do
+    local ToggleUI = game.CoreGui:FindFirstChild("MyToggle") 
+    if ToggleUI then 
+    ToggleUI:Destroy() 
+    end
+end
+
+local MyToggle = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+local UICorner = Instance.new("UICorner")
+
+--Properties:
+
+MyToggle.Name = "MyToggle"
+MyToggle.Parent = game.CoreGui
+MyToggle.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+ImageButton.Parent = MyToggle
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(0.156000003, 0, -0, 0)
+ImageButton.Size = UDim2.new(0, 50, 0, 50)
+ImageButton.Image = "rbxassetid://96764407519217"
+ImageButton.MouseButton1Click:Connect(function()
+game.CoreGui:FindFirstChild("ScreenGui").Enabled = not game.CoreGui:FindFirstChild("ScreenGui").Enabled
+end)
+
+
+UICorner.CornerRadius = UDim.new(0, 10)
+UICorner.Parent = ImageButton
